c4c6db1447e7850e1e48083359e8442ae0796479
unknown <duongvicuong7689@gmail.com>
2025-10-28 16:39:33 +0700

    du an web hoc tren lop

---
+++ WebApplication1/WebApplication1/Controllers/TrangChuController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        public ActionResult Testjvs()
+        {
+            return View();
+        }
+        public ActionResult TestGrid()
+        {
+            return View();
+        }
+        public ActionResult trangchu()
+        {
+            return View();
+        }
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Gioithieu()
+        {
+            return View();
+        }
+        public ActionResult Sanpham()
+        {
+            return View();
+        }
+    }
+}
